4b8911a913b7c037c53e2117f33128f1c58bae9a
Joe Yeakel <josephyeakel@gmail.com>
2025-04-01 14:37:52 -0400

    Clean up code organization

--- public/client.js
+++ public/client.js
@@ -1,97 +1,80 @@
-var pascalRowNumber = 1
+function generatePascalTriangle(canvasId, interval = 500) {
+  let pascalRowNumber = 1;
 
-var triangle = function() {
-  let canvas = document.getElementById('content');
-  let ctx = canvas.getContext('2d');
-  //let pascalRowNumber = //Math.floor(Math.random() * 40);
-  let binaryRow = binarizeArray(pascalRow(pascalRowNumber));
-  let dimension = (binaryRow.length % 2) ? 51 : 50;
-  let pixelSize = 16;
-  let pixelIndex = 0; // This shouldn't be a global variable
-  
   function pascalRow(n) {
     let line = [1];
     for (let i = 0; i < n; i++) {
-        line.push(line[i] * (n-i) / (i+1));
+      line.push(Math.floor(line[i] * (n - i) / (i + 1)));
     }
-    return line
+    return line;
   }
-  
-  function binarizeArray(row) {
-    let binaryRow = [];
-    for (let i = 0; i < row.length; i++) {
-        binaryRow.push(row[i].toString(2));
-    }
-    return binaryRow;
-  }
-  
-  function columnContainsBinary(i) {
-    let currentColumn = (i % dimension);
-    let currentRow = Math.floor(i / dimension);
-    if (currentColumn >= (dimension - binaryRow.length)/2 && 
-        currentColumn < dimension - (dimension - binaryRow.length)/2) {
-      return true;
-    }
-  }
-  
-  function rowContainsBinary(i) {
-    let longestBinaryString = binaryRow.reduce((r,s) => r > s.length ? r : s.length, 0);
-    let currentRow = Math.floor(i / dimension);
-    if (dimension - longestBinaryString <= currentRow){
-      return true;
-    }
-  }
-  
-  function getColorScheme() {
-      return [(Math.round(Math.random()* 127) + 127).toString(16), 
-              (Math.round(Math.random()* 127) + 127).toString(16)
-             ];
-  }
-  
-  function getBinaryStringIndex(currentRow, ix) {
-    if (dimension - currentRow <= binaryRow[ix].length) {
-      return Math.abs(dimension - currentRow - binaryRow[ix].length)
-    } else {
-      undefined;
-    }
-  }
-  
-  function getPixelColor(i, bi, colors) {
-    let color
-    if (columnContainsBinary(i) && rowContainsBinary(i)) {
-      color = (binaryRow[pixelIndex][bi] == '1')
-        ? '#' + colors[0] + (Math.round(Math.random()* 127) + 127).toString(16) + colors[1] 
-        : 'black';
-      pixelIndex = (pixelIndex == binaryRow.length - 1) ? 0 : pixelIndex + 1;
-      } else {
-        color = 'black';
-      } 
-    return color
-  }
-    
-  function drawTriangle() {
-    canvas.width = dimension * pixelSize;
-    canvas.height = dimension  * pixelSize;
-    let baseColors = getColorScheme(); 
-    for (let i = 0; i < dimension * dimension; i++) {
-      let currentColumn = (i % dimension);
-      let currentRow = Math.floor(i / dimension);
-      let X = currentColumn * pixelSize;
-			let Y = currentRow * pixelSize;
-      let bIndex = getBinaryStringIndex(currentRow, pixelIndex)
-      
-      ctx.fillStyle = getPixelColor(i, bIndex, baseColors);
-			ctx.fillRect(X, Y, pixelSize, pixelSize);
-		}
-  }
-  drawTriangle()
-  if (pascalRowNumber < 50) {
-    pascalRowNumber++
-  } else {
-    pascalRowNumber = 1;
-  }
-};
 
-(function(){
-  setInterval(triangle, 300);
-})();
+  function binarizeArray(row) {
+    return row.map(num => num.toString(2));
+  }
+
+  function getColorScheme() {
+    return [
+      (Math.round(Math.random() * 127) + 127).toString(16),
+      (Math.round(Math.random() * 127) + 127).toString(16),
+    ];
+  }
+
+  function drawTriangle() {
+    let canvas = document.getElementById(canvasId);
+    let ctx = canvas.getContext('2d');
+    let binaryRow = binarizeArray(pascalRow(pascalRowNumber));
+    let dimension = (binaryRow.length % 2) ? 51 : 50;
+    let pixelSize = 16;
+    let pixelIndex = 0;
+
+    canvas.width = dimension * pixelSize;
+    canvas.height = dimension * pixelSize;
+    let baseColors = getColorScheme();
+
+    function columnContainsBinary(i) {
+      let currentColumn = i % dimension;
+      return currentColumn >= (dimension - binaryRow.length) / 2 &&
+        currentColumn < dimension - (dimension - binaryRow.length) / 2;
+    }
+
+    function rowContainsBinary(i) {
+      let longestBinaryString = binaryRow.reduce((r, s) => Math.max(r, s.length), 0);
+      let currentRow = Math.floor(i / dimension);
+      return dimension - longestBinaryString <= currentRow;
+    }
+
+    function getBinaryStringIndex(currentRow, index) {
+      return dimension - currentRow <= binaryRow[index].length ?
+        Math.abs(dimension - currentRow - binaryRow[index].length) :
+        undefined;
+    }
+
+    function getPixelColor(i, binaryIndex) {
+      if (columnContainsBinary(i) && rowContainsBinary(i)) {
+        let color = binaryRow[pixelIndex][binaryIndex] === '1' ?
+          '#' + baseColors[0] + (Math.round(Math.random() * 127) + 127).toString(16) + baseColors[1] :
+          'black';
+        pixelIndex = (pixelIndex + 1) % binaryRow.length;
+        return color;
+      }
+      return 'black';
+    }
+
+    for (let i = 0; i < dimension * dimension; i++) {
+      let currentColumn = i % dimension;
+      let currentRow = Math.floor(i / dimension);
+      let x = currentColumn * pixelSize;
+      let y = currentRow * pixelSize;
+      let binaryIndex = getBinaryStringIndex(currentRow, pixelIndex);
+      ctx.fillStyle = getPixelColor(i, binaryIndex);
+      ctx.fillRect(x, y, pixelSize, pixelSize);
+    }
+
+    pascalRowNumber = (pascalRowNumber < 50) ? pascalRowNumber + 1 : 1;
+  }
+
+  setInterval(drawTriangle, interval);
+}
+
+generatePascalTriangle('content');
